4f267026b2706a0ab22d95052ed2bfd65bd8dc84
Luee kasaudhan <105124284+LueeKasaudhan@users.noreply.github.com>
2026-09-15 08:40:11 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashmap;
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if(hashmap.count(complement)) return {hashmap[complement], i};
+            hashmap[nums[i]] = i;
+        }
+        return {};
+    }
+};
